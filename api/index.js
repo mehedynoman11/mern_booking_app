a2e8a8118f0886384c60285653c21a91e42132d1
mehedynoman11 <mehedynoman11@gmail.com>
2024-02-11 10:37:44 +0600

    add mongoose .env

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI, {
+})
+.then(() => {
+    console.log("MongoDB is Connected!");
+})
+.catch(err => {
+    console.error(err);
+});
 
 const app = express();
 
