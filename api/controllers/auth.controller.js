75f4dfe53eb98f4cc4d83f6aaa7648ea5331a561
mehedynoman11 <mehedynoman11@gmail.com>
2024-02-12 16:18:37 +0600

    error show into message

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,14 @@
 import User from "../models/user.model.js";
 import bcrypt from 'bcryptjs'
 
-export const signup =  async (req, res) => {
-    const {username, email, password} = req.body;
+export const signup = async (req, res) => {
+    const { username, email, password } = req.body;
     const hashedPassword = bcrypt.hashSync(password, 10);
-    const newUser = new User({username, email, password: hashedPassword});
-    await newUser.save();
-    res.status(203).json("user created successfully!")
+    const newUser = new User({ username, email, password: hashedPassword });
+    try {
+        await newUser.save();
+        res.status(203).json("user created successfully!")
+    } catch (error) {
+        res.status(501).json(error.message);
+    }
 }
