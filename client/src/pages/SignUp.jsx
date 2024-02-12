15e174b1604eb0189a332d8620e21b58d2cc4490
mehedynoman11 <mehedynoman11@gmail.com>
2024-02-12 19:28:43 +0600

    signUp page complete 2

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,11 @@
-import { data } from "autoprefixer";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({
@@ -32,6 +32,9 @@ export default function SignUp() {
         setLoading(false);
         return;
       }
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in')
       
     } catch (error) {
       setLoading(false);
