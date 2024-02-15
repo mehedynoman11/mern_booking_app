ae0bb0e41ef814d99b6019def900d9b4eb520562
mehedynoman11 <mehedynoman11@gmail.com>
2024-02-15 12:45:11 +0600

    profile ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -22,25 +22,29 @@ export default function Profile() {
           type="text"
           placeholder="Username"
           id="username"
-          className="border p-3 rounded-lg w-full mb-4"
+          className="border p-3 rounded-lg w-full mb-4 bg-green-100"
         />
         <input
           type="email"
           placeholder="Email"
           id="email"
-          className="border p-3 rounded-lg w-full mb-4"
+          className="border p-3 rounded-lg w-full mb-4 bg-green-100"
         />
         <input
           type="password"
           placeholder="Password"
           id="password"
-          className="border p-3 rounded-lg w-full mb-4"
+          className="border p-3 rounded-lg w-full mb-4 bg-green-100"
         />
         <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-lg">
           Update
         </button>
       </div>
     </form>
+    <div className='flex justify-between mt-5'>
+      <span className='text-white font-semibold cursor-pointer bg-rose-500 p-2 rounded-xl'>Delete Account</span>
+      <span className='text-white font-semibold cursor-pointer bg-rose-500 p-2 rounded-xl'>Sign out</span>
+    </div>
   </div>
 </div>
 
