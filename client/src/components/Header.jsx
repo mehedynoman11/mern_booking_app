36045e5f3b6a658560a566794bd19877032c4ab6
mehedynoman11 <mehedynoman11@gmail.com>
2024-02-21 14:00:49 +0600

    search header form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,119 +1,33 @@
-// import React, { useState } from "react";
-// import { FaSearch, FaBars, FaTimes } from "react-icons/fa";
-// import { Link } from "react-router-dom";
-// import { useSelector } from "react-redux";
-
-// export default function Header() {
-//   const { currentUser } = useSelector((state) => state.user);
-//   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-//   const toggleMenu = () => {
-//     setIsMenuOpen(!isMenuOpen);
-//   };
-
-//   return (
-//     <header>
-//       <div className="flex justify-between items-center mx-auto p-3 bg-green-200">
-//         <h1 className="font-bold text-2xl sm:text-xl flex flex-wrap">
-//           Arena-State
-//         </h1>
-//         <div className="sm:hidden">
-//           {isMenuOpen ? (
-//             <FaTimes className="text-2xl cursor-pointer" onClick={toggleMenu} />
-//           ) : (
-//             <FaBars className="text-2xl cursor-pointer" onClick={toggleMenu} />
-//           )}
-//         </div>
-//         <div
-//           className={`hidden sm:flex  items-center ${
-//             isMenuOpen ? "flex" : "hidden"
-//           }`}
-//         >
-//           <ul className="flex gap-6 sm:text-xl font-semibold">
-//             <Link to={"/"} className="text-bgDark hover:underline">
-//               Home
-//             </Link>
-//             <Link to={"/about"} className="text-bgDark hover:underline">
-//               About
-//             </Link>
-
-//             {/* <Link
-//               to={currentUser ? "/profile" : "/sign-in"}
-//               className="text-bgDark hover:underline"
-//             >
-//               {currentUser ? (
-//                 <img
-//                   className="rounded-full h-7 w-7 object-cover"
-//                   src={currentUser.avatar}
-//                   alt=""
-//                 />
-//               ) : (
-//                 "Sign in"
-//               )}
-//             </Link> */}
-//           </ul>
-//           <form className="bg-slate-100 p-3 rounded-lg flex items-center ml-3">
-//             <input
-//               type="text"
-//               placeholder="Search..."
-//               className="bg-transparent focus:outline-none w-24 sm:w-64"
-//             />
-//             <FaSearch className="text-slate-600" />
-//           </form>
-//         </div>
-//         <Link
-//               to={currentUser ? "/profile" : "/sign-in"}
-//               className="text-bgDark hover:underline"
-//             >
-//               {currentUser ? (
-//                 <img
-//                   className="rounded-full h-7 w-7 object-cover"
-//                   src={currentUser.avatar}
-//                   alt=""
-//                 />
-//               ) : (
-//                 "Sign in"
-//               )}
-//             </Link>
-//       </div>
-      
-//       {isMenuOpen && (
-//         <ul className="sm:hidden bg-green-200 p-3">
-//           <Link to={"/"} className="text-bgDark block py-2 hover:underline">
-//             Home
-//           </Link>
-//           <Link
-//             to={"/about"}
-//             className="text-bgDark block py-2 hover:underline"
-//           >
-//             About
-//           </Link>
-//           {/* <Link
-//             to={"/sign-in"}
-//             className="text-bgDark block py-2 hover:underline"
-//           >
-//             Sign In
-//           </Link> */}
-          
-//         </ul>
-//       )}
-//     </header>
-//   );
-// }
-
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaSearch, FaBars, FaTimes } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('')
+  const navigate = useNavigate();
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const handleSubmit =(e) => {
+    e.preventDefault()
+    const urlParams = new URLSearchParams(window.location.search)
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  }
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl)
+    }
+  }, [location.search])
   return (
     <header>
       <div className="flex justify-between items-center mx-auto p-4 bg-[#7fa6ea]">
@@ -180,13 +94,19 @@ export default function Header() {
             )}
           </Link>
           </ul>
-          <form className="bg-slate-100 p-3 rounded-lg flex items-center ml-3">
+          <form 
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg 
+          flex items-center ml-3">
             <input
               type="text"
               placeholder="Search..."
               className="bg-transparent focus:outline-none w-24 sm:w-64"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
             />
             <FaSearch className="text-slate-600" />
+            <button></button>
           </form>
         </div>
       </div>
